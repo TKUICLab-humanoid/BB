fd0c33b900b6e1ad92dc957b28de7c1b018002a2
aa <w1649n@gmail.com>
2024-07-02 14:05:45 +0800

    5point motion set

--- BB_currybird.py
+++ BB_currybird.py
@@ -12,24 +12,24 @@ from Python_API import Sendmessage
 
 #======================================================================================
 
-CORRECT       = [0, -100, 0]        #原地踏步修正
-LEFT_CORRECT  = [0, -500, 3]        #左旋修正
-RIGHT_CORRECT = [50, 550, -4]       #右旋修正
+CORRECT       = [-200, -100, 0]        #原地踏步修正
+LEFT_CORRECT  = [-200, -500, 3]        #左旋修正
+RIGHT_CORRECT = [-50, 450, -4]       #右旋修正
 #                 x , y , theta 
 
 #=====================================================================================
 
-BASKET_SIZE_60_90 = [2550, 1120]       #sector 5301                                                     #投籃時測量的籃框距離方法 #五分投籃時站姿高度看籃框size測距離
-FIVEPOINT_HEAD_Y_DEGREE = [1980]      #投出去偏向左邊＝>頭往左轉（大）-朝1960 ;  投出去偏向右邊＝>頭往右轉（小）-朝1940    #投籃前頭會固定一個角度，並扭腰
+BASKET_SIZE_60_90 = [2540, 1050]       #sector 5301                                                     #投籃時測量的籃框距離方法 #五分投籃時站姿高度看籃框size測距離
+FIVEPOINT_HEAD_Y_DEGREE = [1975]      #投出去偏向左邊＝>頭往左轉（大）-朝1960 ;  投出去偏向右邊＝>頭往右轉（小）-朝1940    #投籃前頭會固定一個角度，並扭腰
 THROW_BALL_PLUS = 200                 #line  0   left side 0 right side  4
 #15.9 - 150
 #三用電表15.7以上ˇ
-#======================================================================================
+#=====================================================================================
 
-CATCH_BALL_LINE = [1800, 1624, 1550]            # slow_degree,stop_degree,backward_degree
-TWO_POINT_LINE  = [1800, 1750, 1650]            # slow_degree,stop_degree,backward_degree
-THREE_POINT_LINE = [1600, 2200, 2250, 2200]     # forward_stop_size < forward_slow_size < backward_slow_size < backward_stop_size #上下上下-30
-FIVE_POINT_LINE  = [750, 900, 950, 900]         # forward_stop_size < forward_slow_size < backward_slow_size < backward_stop_size
+CATCH_BALL_LINE = [1800, 1622, 1550]            # slow_degree,stop_degree,backward_degree
+TWO_POINT_LINE  = [1800, 1700, 1600]            # slow_degree,stop_degree,backward_degree
+THREE_POINT_LINE = [1600, 2100, 2160, 2300]     # forward_stop_size < forward_slow_size < backward_slow_size < backward_stop_size #上下上下-30
+FIVE_POINT_LINE  = [700, 800, 860, 1000]         # forward_stop_size < forward_slow_size < backward_slow_size < backward_stop_size
 #注意 size數值調越大會離籃框越近！！！
 
 send = Sendmessage()
@@ -149,7 +149,7 @@ class BasketBall():
         #send.sendBodySector(9) #讓手回歸自我們的初始手部位置,原是AR的
         time.sleep(0.05)
         send.sendBodySector(5)   #步態調整
-        send.sendBodySector(6)   #步態調整
+        ##send.sendBodySector(6)   #步態調整
         time.sleep(0.05)
         self.step = 'find_ball'
         
@@ -348,7 +348,7 @@ class BasketBall():
                         rospy.logdebug(f'五分球動作預備')
                         rospy.logdebug(f'手臂往後舉')
                         send.sendBodySector(5301)
-                        time.sleep(8)   
+                        time.sleep(11)   
                         rospy.logdebug(f'頭部調整') 
                         rospy.logdebug(f'頭部水平旋轉調整')                                              
                         motor.move_head(1, FIVEPOINT_HEAD_Y_DEGREE[0], 880, 880, 50)
@@ -484,7 +484,7 @@ class BasketBall():
                     if abs(motor.head_horizon-1910) > 4: 
                         rospy.loginfo(f'匡不在視野中間->貓頭鷹修腰')
                         # rospy.loginfo(f"motor.head_horizon = {motor.head_horizon}")
-                        motor.Owl_Rotate(1910)  
+                        motor.Owl_Rotate(1915)  
 
                     else:
                         time.sleep(1)
@@ -515,7 +515,7 @@ class BasketBall():
                 rospy.logdebug(f'五分球動作預備')
                 rospy.logdebug(f'手臂往後舉')
                 send.sendBodySector(5301)
-                time.sleep(8)   
+                time.sleep(11)   
                 rospy.logdebug(f'頭部調整') 
                 rospy.logdebug(f'頭部水平旋轉調整')                                              
                 motor.move_head(1, FIVEPOINT_HEAD_Y_DEGREE[0], 880, 880, 50)
